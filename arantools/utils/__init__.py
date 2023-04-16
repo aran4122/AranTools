ba39e5a0fb3a745a42f8f3bbdd2ab5ca1fe4cd7a
Aran Gao <aran4122@126.com>
2023-04-16 22:42:52 +0800

    add io.py

---
+++ arantools/utils/__init__.py
@@ -0,0 +1,16 @@
+from .io import (prepare_input_path, prepare_output_path, read_json, read_list,
+                 read_pickle, read_yaml, write_json, write_list, write_pickle,
+                 write_yaml)
+
+__all__ = [
+    "prepare_input_path",
+    "prepare_output_path",
+    "read_list",
+    "write_list",
+    "read_json",
+    "write_json",
+    "read_yaml",
+    "write_yaml",
+    "read_pickle",
+    "write_pickle",
+]
