961ea24db53d477eef900a6c2b26685cb260d104
Aran Gao <aran4122@126.com>
2023-09-17 00:24:50 +0800

    add srt2ass.py

--- tools/translate/translate_by_faster_whisper.py
+++ tools/translate/translate_by_faster_whisper.py
@@ -1,33 +1,10 @@
 import argparse
 import os.path as osp
 
+from common import ASS_HEADER
 from faster_whisper import WhisperModel
 
 from arantools.utils import write_list
-
-ASS_HEADER = """[Script Info]
-; This is an Advanced Sub Station Alpha v4+ script.
-Title:
-ScriptType: v4.00+
-Collisions: Normal
-PlayDepth: 0
-ScaledBorderAndShadow: yes
-PlayResX: 1280
-PlayResY: 720
-
-[Aegisub Project Garbage]
-Audio File: {}
-Video File: {}
-
-[V4+ Styles]
-Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,思源黑体 CN Heavy,54,&H00FFFFFF,&H000019FF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,1,2.66667,2,2,7,7,7,1
-Style: 备注,思源黑体 CN Heavy,50,&H00FFFFFF,&H000019FF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,1,2.66667,2,8,7,7,7,1
-Style: 水印,胡晓波男神体,20,&H32FFFFFF,&H000000FF,&HFF000000,&HFF000000,0,0,0,0,100,100,0,0,1,2,0,7,10,10,10,1
-
-[Events]
-Format: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text
-Dialogue: 0,0:00:00.00,2:00:00.00,水印,,0,0,0,,道德天尊战神菌"""
 
 
 def parse_args():
